5c39b889bf42988a2f2ccbedfb5122f74cd90a22
seitamuro <seitamuro0210@gmail.com>
2024-05-18 08:14:25 +0900

    Initial commit

---
+++ test/cdk-socket-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSocketApi from '../lib/cdk-socket-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-socket-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSocketApi.CdkSocketApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
